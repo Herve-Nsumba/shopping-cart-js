a6d6636c0eabc793c753be455943056406a0ad80
Herve-Nsumba <nsumbaherve11@gmail.com>
2025-08-19 16:51:50 +0200

    second project, shopping-cart-js

---
+++ README.md
@@ -0,0 +1,18 @@
+# Shopping Cart Project
+
+A simple and customizable shopping cart system built for e-commerce product pages. It allows users to add, remove, and update items in the cart, view total price, and proceed to checkout.
+
+## ✨ Features
+
+- Add products to cart
+- Remove products from cart
+- Update item quantity
+- Display total price
+- Responsive design for product pages
+- Persistent cart data (using localStorage/session or backend API)
+- Easy integration into any e-commerce website
+
+## 🛠 Tech Stack
+
+- HTML, CSS, JavaScript (Vanilla JS)
+- LocalStorage for cart persistence
---
+++ main.js
@@ -0,0 +1,61 @@
+let shop = document.getElementById("shop");
+
+let shopItemsData = [
+  {
+    id: "item1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "item2",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "item3",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "item4",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      return `
+  <div id= product-id${id} class="item">
+        <img width="220" src="${img}" alt="" />
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>${price}</h2>
+            <div class="buttons">
+              <i onlick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">0</div>
+              <i onlick="decrement(${id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+    })
+    .join(""));
+};
+generateShop();
+
+let increment = () => {};
+let decrement = () => {};
